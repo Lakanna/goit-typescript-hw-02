cefe56b8c742831dc8571cc3a6fafb5a551e2a42
Hanna Lakus <annalakus@ukr.net>
2024-10-07 20:20:16 +0100

    add types to form

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,21 @@
 import toast from "react-hot-toast";
 import css from "./SearchBar.module.css";
+import { FormEvent } from "react";
 
-export default function SearchBar({ onSubmit }) {
-  function handleSubmit(e) {
+interface SearchBarProps {
+  onSubmit: (img: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    const form = e.target;
-    const formValue = form.elements.search.value.trim();
+    const form = e.target as HTMLFormElement;
+    const inputElement = form.elements.namedItem("search") as HTMLInputElement; // Явно приводимо до HTMLInputElement
+    const formValue = inputElement.value.trim(); // Отримуємо значення поля
+
+    console.dir(form, "form");
+    console.log(formValue, "formValue");
 
     if (formValue === "") {
       toast.error("Please, input value for search");
@@ -32,4 +41,6 @@ export default function SearchBar({ onSubmit }) {
       </form>
     </section>
   );
-}
+};
+
+export default SearchBar;
